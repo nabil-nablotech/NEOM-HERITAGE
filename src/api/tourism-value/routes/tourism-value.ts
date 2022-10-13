2ac402ea45841ed3518c81fddaec7f9f67874679
Ritesh <ritesh.kumar@speridian.com>
2022-10-13 20:31:18 +0530

    Final place collection

--- src/api/tourism-value/routes/tourism-value.ts
+++
@@ -1,7 +0,0 @@
-/**
- * tourism-value router.
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::tourism-value.tourism-value');
--- src/api/tourism-value/services/tourism-value.ts
+++
@@ -1,7 +0,0 @@
-/**
- * tourism-value service.
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::tourism-value.tourism-value');
