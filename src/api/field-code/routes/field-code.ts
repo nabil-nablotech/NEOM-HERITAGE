50a6e00ee443b55c588f11ab00e84c8457fa8aee
Ritesh <ritesh.kumar@speridian.com>
2022-10-13 11:13:10 +0530

    DB collection

---
+++ src/api/field-code/routes/field-code.ts
@@ -0,0 +1,7 @@
+/**
+ * field-code router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::field-code.field-code');
---
+++ src/api/field-code/services/field-code.ts
@@ -0,0 +1,7 @@
+/**
+ * field-code service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::field-code.field-code');
