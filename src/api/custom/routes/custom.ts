16846193074173e3dfe7ab6ceef77396bcddbc5d
Ritesh <ritesh.kumar@speridian.com>
2022-10-25 13:05:42 +0530

    custom api for logout

--- src/api/custom/routes/custom.ts
+++ src/api/custom/routes/custom.ts
@@ -1,27 +1,36 @@
 export default {
   routes: [
     {
-     method: 'PUT',
-     path: '/custom/change-password/:id',
-     handler: 'custom.changePassword',
-     config: {
-       policies: [],
-       middlewares: [],
-     },
-    },
-    {
-      method: 'GET',
-      path: '/custom/search',
-      handler: 'custom.searchCount',
+      method: "PUT",
+      path: "/custom/change-password/:id",
+      handler: "custom.changePassword",
       config: {
         policies: [],
         middlewares: [],
       },
     },
     {
-      method: 'GET',
-      path: '/custom/options',
-      handler: 'custom.refinedSearchOptions',
+      method: "GET",
+      path: "/custom/search",
+      handler: "custom.searchCount",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "GET",
+      path: "/custom/options",
+      handler: "custom.refinedSearchOptions",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "PUT",
+      path: "/custom/update-me",
+      handler: "custom.updateMe",
       config: {
         policies: [],
         middlewares: [],
