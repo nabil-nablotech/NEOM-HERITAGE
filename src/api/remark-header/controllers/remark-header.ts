201c4159d8affa4c13ab532313fd0b488e8cc0fc
Ritesh <ritesh.kumar@speridian.com>
2022-10-28 18:16:01 +0530

    remark collections

---
+++ src/api/remark-header/controllers/remark-header.ts
@@ -0,0 +1,7 @@
+/**
+ *  remark-header controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::remark-header.remark-header');
---
+++ src/api/remark-header/services/remark-header.ts
@@ -0,0 +1,7 @@
+/**
+ * remark-header service.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::remark-header.remark-header');
