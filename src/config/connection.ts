926b0077d3f12554150d0c75bc8d8f737068ac9f
Ritesh <ritesh.kumar@speridian.com>
2022-11-16 14:05:08 +0530

    set env variables

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -2,15 +2,15 @@
 import mysql from "mysql"
 
 var con = mysql.createConnection({
-  host: '127.0.0.1',
-  port: 3306,
-  database: 'neom',
-  user: 'root',
-  password: 'Abate@2022',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
   ssl: false,
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
@@ -18,8 +18,8 @@ con.connect(function(err) {
 
 const fetchPLaces = (cb) => {
   con.query("SELECT * FROM places", function (err, result) {
-    cb(err,result)
+    cb(err, result)
   });
 };
 
-export {fetchPLaces}
+export { fetchPLaces }
